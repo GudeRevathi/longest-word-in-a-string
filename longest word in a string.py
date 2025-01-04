4fff62af6f2fb4a90d5e328846497ceaf4348278
GudeRevathi <126940892+GudeRevathi@users.noreply.github.com>
2025-01-04 15:14:43 +0530

    Update longest word in a string.py

--- longest word in a string.py
+++ longest word in a string.py
@@ -1,3 +1,16 @@
 sentence = input("Enter a sentence: ")
 words = sentence.split()
 print(max(words, key=len))
+
+
+            (or)
+-------------------------------
+
+
+string="revathi gude welcome  aaaaaaaaaa"
+string1=string.split()
+new_str=""
+for word in string1:
+    if len(word)>len(new_str):
+        new_str=word
+print(new_str)
